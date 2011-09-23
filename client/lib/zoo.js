5b8ab4f383984395ebef1e5c8f23c4d4ff79c5fa
Hugo Josefson <hugo@josefson.org>
2011-09-23 11:11:16 +0200

    Animal and AnimalView pass.

--- client/lib/zoo.js
+++ client/lib/zoo.js
@@ -1,7 +1,41 @@
 (function() {
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   $(function() {
-    return window.sanity = function() {
+    window.sanity = function() {
       return 'sane';
     };
+    window.Animal = (function() {
+      __extends(Animal, Backbone.Model);
+      function Animal() {
+        Animal.__super__.constructor.apply(this, arguments);
+      }
+      return Animal;
+    })();
+    return window.AnimalView = (function() {
+      __extends(AnimalView, Backbone.View);
+      function AnimalView() {
+        this.render = __bind(this.render, this);
+        this.initialize = __bind(this.initialize, this);
+        AnimalView.__super__.constructor.apply(this, arguments);
+      }
+      AnimalView.prototype.template = _.template($('#animal-template').html());
+      AnimalView.prototype.tagName = "li";
+      AnimalView.prototype.className = "animal";
+      AnimalView.prototype.initialize = function() {
+        return this.model.bind("change", this.render);
+      };
+      AnimalView.prototype.render = function() {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+      };
+      return AnimalView;
+    })();
   })();
 }).call(this);
--- client/src/zoo.js.coffee
+++ client/src/zoo.js.coffee
@@ -2,3 +2,16 @@ do $ ->
   window.sanity = ->
     'sane'
 
+  class window.Animal extends Backbone.Model
+
+  class window.AnimalView extends Backbone.View
+    template: _.template($('#animal-template').html())
+    tagName: "li"
+    className: "animal"
+
+    initialize: =>
+      @model.bind("change", @render)
+
+    render: =>
+       $(@el).html @template(@model.toJSON())
+       @
